3e880373eb9e600eae75a6e15ff0484d1305da3a
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-08 17:46:53 -0400

    add facebook profile picture url upon log in

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160908170224) do
+ActiveRecord::Schema.define(version: 20160908135949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,17 +33,12 @@ ActiveRecord::Schema.define(version: 20160908170224) do
   create_table "users", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "username"
     t.string   "email"
-    t.string   "password_digest"
+    t.string   "profile_picture_url"
+    t.string   "facebook_user_id"
     t.integer  "points",              default: 0
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
-    t.string   "facebook_user_id"
   end
 
   create_table "votes", force: :cascade do |t|
