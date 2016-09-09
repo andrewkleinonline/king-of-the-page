c707cd38ef2112a94395cefac1f28c514b98083c
Ece Ozalp <github email address>
2016-09-09 15:46:29 -0400

    kinging complete

--- app/models/response.rb
+++ app/models/response.rb
@@ -5,6 +5,23 @@ class Response < ApplicationRecord
   has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  def king_check
+    kinger = 0
+    temp_high_vote = 0
+    prospective_king = []
+    self.prompt.responses.each do |r|
+      kinger += r.votes.count
+      if r.votes.count > temp_high_vote
+        temp_high_vote = r.votes.count
+        prospective_king = r.subject
+      end
+    end
+
+    if kinger == 2
+      prospective_king.make_king
+    end
+  end
+
   def update_votes(user)
     user_voted?(user) ? unvote(user) : vote(user)
     self.king_check
@@ -40,22 +57,6 @@ class Response < ApplicationRecord
     self.votes.find_by(subject_id: user.id).destroy
   end
 
-  def king_check
-    kinger = 0
-    temp_high_vote = 0
-    prospective_king = []
-    self.prompt.responses.each do |r|
-      kinger += r.votes.count
-      if r.votes.count > temp_high_vote
-        temp_high_vote = r.votes.count
-        prospective_king = response.subject
-      end
-    end
-
-    if kinger == 2
-      prospective_king.make_king
-    end
-  end
-
+  
 
 end
