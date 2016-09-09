35541eef16c5c36be17783ce63f9cb82d7e6e08e
Andrew Klein <andrew.klein.j@gmail.com>
2016-09-09 15:30:09 -0400

    rename facebook environment vars

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['APP_ID'], ENV['APP_SECRET'],
+  provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET'],
   scope: 'public_profile,email', display: 'popup',
   info_fields: 'first_name,last_name,email',
   image_size: 'large'
