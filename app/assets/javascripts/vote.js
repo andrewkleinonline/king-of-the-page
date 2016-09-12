995160fd7fa33ed21ae00a60e8c08f6c88be6a52
Ece Ozalp <github email address>
2016-09-12 18:25:29 -0400

    trying removing js

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -4,13 +4,16 @@ $(document).ready(function () {
 });
 
 
+// we have to reimplement voting capabilities because heroku doesn't accept `
+
+
   function voteAjaxSuccessListener () {
     $(".vote-link").on("ajax:complete", function(e, request, status){
       var response = request.responseJSON;
       $("#response_" + response.response_id + " i").attr('class', response.upvote_class);
-      $("#response_" + response.response_id + " .vote-count").text(`${response.votes_number} Votes`);
-      $("#response_" + response.response_id + " #subject-points").text(`${response.subject_points} Points`);
-      $("#king-points").text(`${response.king_points}`)
+      $("#response_" + response.response_id + " .vote-count").text(response.votes_number + ' Votes');
+      $("#response_" + response.response_id + " #subject-points").text(response.subject_points} + '  Points');
+      $("#king-points").text(response.king_points})
     });
   }
 
