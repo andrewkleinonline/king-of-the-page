d6033f19dde4307c664c9cb2ebe6c0fee3c2175e
KhaledaNYC <khaleda.khan87@gmail.com>
2016-09-13 10:41:30 -0400

    fixed to move var to top

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -1,14 +1,12 @@
+var voteAjaxSuccessListener = function() {
+ $(".vote-link").on("ajax:complete", function(e, request, status){
+   var response = request.responseJSON;
+   $("#response_" + response.response_id + " i").attr('class', response.upvote_class);
+   $("#response_" + response.response_id + " .vote-count").text(response.votes_number + "Votes");
+   $("#response_" + response.response_id + " #subject-points").text(response.subject_points + "Points");
+   $("#king-points").text(response.king_points)
+ });
+}
 $(document).ready(function () {
   voteAjaxSuccessListener();
 });
-
-
-   var voteAjaxSuccessListener = function() {
-    $(".vote-link").on("ajax:complete", function(e, request, status){
-      var response = request.responseJSON;
-      $("#response_" + response.response_id + " i").attr('class', response.upvote_class);
-      $("#response_" + response.response_id + " .vote-count").text(response.votes_number + "Votes");
-      $("#response_" + response.response_id + " #subject-points").text(response.subject_points + "Points");
-      $("#king-points").text(response.king_points)
-    });
-  }
