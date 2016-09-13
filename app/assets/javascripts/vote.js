40fef0d3f7898a687dd7c003a6564e0ab09a4528
Ece Ozalp <github email address>
2016-09-13 10:23:07 -0400

    trying concat for js

--- app/assets/javascripts/vote.js
+++ app/assets/javascripts/vote.js
@@ -7,8 +7,8 @@ $(document).ready(function () {
     $(".vote-link").on("ajax:complete", function(e, request, status){
       var response = request.responseJSON;
       $("#response_" + response.response_id + " i").attr('class', response.upvote_class);
-      $("#response_" + response.response_id + " .vote-count").text(`${response.votes_number} Votes`);
-      $("#response_" + response.response_id + " #subject-points").text(`${response.subject_points} Points`);
-      $("#king-points").text(`${response.king_points}`)
+      $("#response_" + response.response_id + " .vote-count").text(response.votes_number + "Votes");
+      $("#response_" + response.response_id + " #subject-points").text(response.subject_points + "Points");
+      $("#king-points").text(response.king_points)
     });
   }
