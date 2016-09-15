02ee936684666ae781cd04b5398eabbdf8112c62
KhaledaNYC <khaleda.khan87@gmail.com>
2016-09-15 09:43:15 -0400

    created response facotry and specs test are failing

--- spec/models/response_spec.rb
+++ spec/models/response_spec.rb
@@ -6,8 +6,13 @@ require 'spec_helper'
 
 describe Response do
 
-  it 'has a subject' do 
-    resp = create(:response)
+  it 'has a subject' do
+    res = create(:response)
+    expect(resp.subject_id).not_to eq(nil)
+  end
+
+  it "has content" do
+    res = create(:response)
     expect(resp.subject_id).not_to eq(nil)
   end
 
